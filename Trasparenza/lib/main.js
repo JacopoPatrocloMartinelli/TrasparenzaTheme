c3fd84154d60f962a5aec58306159c03ef7d7d7e
JacopoPatrocloMartinelli <jacopomartinelli@MacBook-Pro-di-Jacopo.local>
2017-11-16 12:01:30 +0100

    add rotation on image and style menu

--- Trasparenza/lib/main.js
+++ Trasparenza/lib/main.js
@@ -5,7 +5,14 @@ jQuery(document).ready(function($) {
 
   $(".father-menu").children("ul").children(".father-menu").slideToggle(0,function() {});
   $(".father-menu").click(function(){
-    $(this).children("ul").children(".father-menu").slideToggle(200,function() {});
+    $(this).children("ul").children(".father-menu").slideToggle(200,function(){});
+    if ( $(this).children('img').hasClass('is_rotate') ){
+      $(this).children('img').removeClass('is_rotate');
+      AnimateToNormal($(this).children('img'));
+    }else {
+      $(this).children('img').addClass('is_rotate');
+      AnimateRotate(180,$(this).children('img'));
+    }
   })
 
   var url = document.URL;
@@ -13,17 +20,51 @@ jQuery(document).ready(function($) {
 
   $(".father-menu").children("ul").children(".father-menu").each(function(){
     var currentUrl = $(this).children("a").attr("href");
-    console.log(currentUrl);
     var lastPart = currentUrl.substr(currentUrl.lastIndexOf('/') + 1);
     if (lastPart == currentPage) {
       $(this).addClass("selected");
+      $(this).parent().parent().children('img').addClass('is_rotate');
+      AnimateRotate(180,$(this).parent().parent().children('img'));
       $(this).parent().children().each(function(){
+        $(this).parent().parent().addClass('father-menu-with-secected-child');
         $(this).toggle();
       });
     }
   })
 
 });
+
+function AnimateToNormal($elem) {
+    // we use a pseudo object for the animation
+    // (starts from `0` to `angle`), you can name it as you want
+    jQuery({deg: 180}).animate({deg: 0}, {
+        duration: 500,
+        step: function(now) {
+            // in the step-callback (that is fired each step of the animation),
+            // you can use the `now` paramter which contains the current
+            // animation-position (`0` up to `angle`)
+            $elem.css({
+                transform: 'rotate(' + now + 'deg)'
+            });
+        }
+    });
+}
+
+function AnimateRotate(angle,$elem) {
+    // we use a pseudo object for the animation
+    // (starts from `0` to `angle`), you can name it as you want
+    jQuery({deg: 0}).animate({deg: angle}, {
+        duration: 500,
+        step: function(now) {
+            // in the step-callback (that is fired each step of the animation),
+            // you can use the `now` paramter which contains the current
+            // animation-position (`0` up to `angle`)
+            $elem.css({
+                transform: 'rotate(' + now + 'deg)'
+            });
+        }
+    });
+}
 
 function pressAndGo(url) {
   window.location.href = url;
